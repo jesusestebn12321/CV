f59c1056d0b94038a8932a8741f1a25d4c04d9a2
Estebn12321 <jesusesteban12321@gmail.com>
2018-09-16 16:50:06 -0700

    animation

--- views/indexViews.php
+++ views/indexViews.php
@@ -243,13 +243,21 @@ include'views/template/userPlantilla.php';
 				<h2>Esperiencia Laboral <i class="fa fa-briefcase"></i></h2>
 				<p class="app-separador-2" style="width: 350px"></p>
 				<ul class="list-group">
-					<li class="list-group-item">
-						<div><h3>QuaTravel</h3></div><p class="app-separador-1" style="width:130px;"></p>
+					<li class="list-group-item" id="app-titulo-li">
+						<div><h3>QuaTravel</h3></div>
 						<div class="app-badge"><span class="badge"><h3 id="app-con-caja">1 Mes</h3></span></div>
+					</li>
+					<li class="list-group-item"id="app-lista-none">
 						<div>Sistema Back-End de oficina, para la empresa Quatravel</div>
-						<div>Elaborado en laravel</div>
-						<div>Front-End: <br>&nbsp;Diseño de vista para el dicho sistema de esta empresa de agencia de viaje de Perú.</div>
-						<div>Back-End: <br>&nbsp;Desarrollo logico del sistema varios CRUD en este sistema.</div>
+					</li>
+					<li class="list-group-item"id="app-lista-none">
+						<div>Desarrollo en laravel</div>						
+					</li>
+					<li class="list-group-item"id="app-lista-none">
+						<div>Back-End: <br>&nbsp;Desarrollo logico del sistema varios CRUD en este sistema.</div>					
+					</li>
+					<li class="list-group-item" id="app-lista-none">
+						<div>Front-End: <br>&nbsp;Diseño de vista para el dicho sistema de esta empresa de agencia de viaje de Perú.</div>						
 					</li>
 				</ul>
 			</div>
